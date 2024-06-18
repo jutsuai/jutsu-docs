fb04e2913682c63f3bbf3105012d4ed63e0976a9
Xaxis <william.neeley@gmail.com>
2024-06-18 13:26:01 -0800

    Fix tracking cache

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/wilneeley/Projects/jutsu/jutsu-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/wilneeley/Projects/jutsu/jutsu-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/wilneeley/Projects/jutsu/jutsu-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/wilneeley/Projects/jutsu/jutsu-docs/src/css/custom.css"),
-];
